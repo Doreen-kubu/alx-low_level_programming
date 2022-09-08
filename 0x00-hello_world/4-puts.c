03181fe833c6e3d40214f70abcfd425bbfcd0d69
Doreen-kubu <doreennkubu@gmail.com>
2022-09-08 19:59:11 +0300

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    /*string initialization*/
+    char message[] = "\"Programming is like building a multilingual puzzle"; /* how to display quotation marks?*/
+    puts(message);
+    return (0);
+}
