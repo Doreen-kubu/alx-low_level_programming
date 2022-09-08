9b50b9f12b69add38b0365b5caaa6dbeaca55a39
Doreen-kubu <doreennkubu@gmail.com>
2022-09-08 22:16:30 +0300

    fixed DOS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main starts reading from here
+ *
+ * return:0 when file is sucessfully executed
+ *
+ */
 {
-    /*string initialization*/
-    char message[] = "\"Programming is like building a multilingual puzzle"; /* how to display quotation marks?*/
-    puts(message);
-    return (0);
+char message[] = "\"Programming is like building a multilingual puzzle";
+/* how to display quotation marks?*/
+puts(message);
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main(void)
+/**
+ * main: oh dear don't we love redundancies
+ * return: 0 another day
+ */
+int main(void) /*main = starts reading, return = 0*/
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n ");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n ");
+return (0);
 }
