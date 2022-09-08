37d7f8ef54991fe2f2d0953ddf9157741a3d4015
Doreen-kubu <doreennkubu@gmail.com>
2022-09-08 22:29:50 +0300

    4 and 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main: oh dear don't we love redundancies
  * return: 0 another day
  */
-int main(void) /*main = starts reading, return = 0*/
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n ");
 return (0);
