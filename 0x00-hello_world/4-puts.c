eee2804b2aa68995a42b1dd0251d73bccee4c12e
Doreen-kubu <doreennkubu@gmail.com>
2022-09-11 17:09:52 +0300

    4-puts fixed commas

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle");
 /* how to display quotation marks?*/
 return (0);
 }
