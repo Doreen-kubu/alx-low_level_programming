1813fdb06ad7ccd689d757cca2138dbb520db691
Doreen-kubu <111055573+Doreen-kubu@users.noreply.github.com>
2022-09-14 18:56:13 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,24 +14,32 @@ int x, y, product;
  */
 for (x = 0; x <= 9; x++)
 {
+for (y = 0; y <= 9; y++)
+{
 product = (x * y);
-if (x == 0)
-_putchar ('0');
+  
+if (y == 0)
+_putchar ('0' + product);
 
 else if (product < 10)
 {
-_putchar (',');
 _putchar (' ');
-_putchar (' ');
-_putchar ((product % 10) + '0');
+_putchar ('0' + product)
 }
-else
+  
+ else
 {
-_putchar (',');
-_putchar (' ');
-_putchar((product / 10) + '0');
-_putchar ((product % 10) + '0');
+_putchar('0' + product / 10);
+_putchar('0' + product % 10);
+}
+if (y < 9)
+{
+_putchar(',');
+_putchar(' ');
 }
 }
 _putchar('\n');
+
+}
+
 }
