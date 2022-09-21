aa6261300ed0bd67c27eb45259a2f135d378e141
Doreen-kubu <111055573+Doreen-kubu@users.noreply.github.com>
2022-09-21 18:30:15 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,28 +6,23 @@
  */
 void rev_string(char *s)
 {
-int full, x, y;
-char tmp, *str;
+	char tmp;
+	int i, len, len1;
 
-while (full >= 0)
-{
-if (s[full] == '\0')
-break;
-full++;
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
-
-str = s;
-
-for (x = 0; x < (full - 1); x++)
-{
-for (y = x + 1; y > 0; y--)
-{
-tmp = *(str + y);
-*(str + y) = *(str + (y - 1));
-*(str + (y - 1)) = tmp;
-}
-
-}
-
-}
-
