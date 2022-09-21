5f3942f213b3592087157538ae498809e51809d5
Doreen-kubu <111055573+Doreen-kubu@users.noreply.github.com>
2022-09-21 17:48:38 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main : generates keygen.
- * return : 0 always.
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
