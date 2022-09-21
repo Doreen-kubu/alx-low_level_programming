5203b22774cce1b40fdf1d1961651a4bd7f30504
Doreen-kubu <111055573+Doreen-kubu@users.noreply.github.com>
2022-09-21 18:58:00 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,36 +6,43 @@
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-int i;
-int h;
-int p;
+int i, d, n, len, f, digit;
 
-h = 0;
-p = -1;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
 
-for (i = 0; s[i] != '\0'; i++)
+while (s[len] != '\0')
+len++;
+
+while (i < len && f == 0)
 {
 if (s[i] == '-')
-p *= -1;
+++d;
 
-if (s[i] > 47 && s[i] < 58)
+if (s[i] >= '0' && s[i] <= '9')
 {
-if (h < 0)
-h = (h * 10) - (s[i] - '0');
-
-else
-h = (s[i] - '0') * -1;
-
-if (s[i + 1] < 48 || s[i + 1] > 57)
+digit = s[i] - '0';
+if (d % 2)
+    
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
+f = 0;
+  
+}
+i++;
 }
 
-if (p < 0)
-h *= -1;
+if (f == 0)
+return (0);
 
-return (h);
-
-
+return (n);
+}
