2d2b85f375201ef06c986f52fd702ce4c5274e65
Doreen-kubu <doreennkubu@gmail.com>
2022-10-14 11:10:27 +0300

    ff

--- 0x0F-functions_pointers/2-int_index.c
+++ 0x0F-functions_pointers/2-int_index.c
@@ -12,16 +12,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
+int index;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
+if (array == NULL || cmp == NULL)
+return (-1);
 
-	for (index = 0; index < size; index++)
-	{
-		if (cmp(array[index]) != 0)
-			return (index);
-	}
+for (index = 0; index < size; index++)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
 
-	return (-1);
+return (-1);
 }
